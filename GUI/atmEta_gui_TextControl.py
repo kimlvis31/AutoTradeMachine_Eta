4e8af864d1a3ef5cb1106478f35659c9700ac903
Bumsu Kim <kimlvis31@gmail.com>
2026-03-15 20:06:32 +0900

    remove: removed pywin32 library as it is no longer needed

--- GUI/atmEta_gui_TextControl.py
+++ GUI/atmEta_gui_TextControl.py
@@ -1,7 +1,6 @@
 from GUI import atmEta_gui_HitBoxes
 
 import pyglet
-#import win32clipboard #DEPRECATED
 import pyperclip
 
 CHARTABLE_LOWER = {48: '0', 49: '1', 50: '2', 51: '3', 52: '4', 53: '5', 54: '6', 55: '7', 56: '8', 57: '9',
