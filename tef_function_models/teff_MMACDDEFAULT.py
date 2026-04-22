d59c8efeb62acafd51cca18bb94652fd28734c64
Bumsu Kim <kimlvis31@gmail.com>
2026-04-22 17:18:40 +0900

    update: finalize MMACDDEFAULT model; tefValue is determined proportionally to the distance from the delta parameter, and downHill threshold is employed, starting to decrease only once the threshold is hit, instead of always

--- tef_function_models/teff_MMACDDEFAULT.py
+++ tef_function_models/teff_MMACDDEFAULT.py
@@ -4,11 +4,12 @@ import math
 FUNCTION MODEL: MMACDDEFAULT
 """
 
-DESCRIPTOR = [{'name': 'alpha',         'defaultValue': 1.0000,   'isAcceptable': lambda x: (( 0.0001 <= x)   and (x <= 5.0000)),   'str_to_val': lambda x: round(float(x), 4), 'val_to_str': lambda x: f"{x:.4f}"},
-              {'name': 'beta',          'defaultValue': 1.00,     'isAcceptable': lambda x: (( 1.00   <= x)   and (x <= 10.00)),    'str_to_val': lambda x: round(float(x), 2), 'val_to_str': lambda x: f"{x:.2f}"},
-              {'name': 'delta',         'defaultValue': 0.0000,   'isAcceptable': lambda x: ((-1.0000 <= x)   and (x <= 1.0000)),   'str_to_val': lambda x: round(float(x), 4), 'val_to_str': lambda x: f"{x:.4f}"},
-              {'name': 'shortStrength', 'defaultValue': 1.000000, 'isAcceptable': lambda x: (( 0.000000 <= x) and (x <= 1.000000)), 'str_to_val': lambda x: round(float(x), 6), 'val_to_str': lambda x: f"{x:.6f}"},
-              {'name': 'longStrength',  'defaultValue': 1.000000, 'isAcceptable': lambda x: (( 0.000000 <= x) and (x <= 1.000000)), 'str_to_val': lambda x: round(float(x), 6), 'val_to_str': lambda x: f"{x:.6f}"}]
+DESCRIPTOR = [{'name': 'alpha',             'defaultValue': 1.0000,   'isAcceptable': lambda x: (( 0.0001 <= x)   and (x <= 5.0000)),   'str_to_val': lambda x: round(float(x), 4), 'val_to_str': lambda x: f"{x:.4f}"},
+              {'name': 'beta',              'defaultValue': 1.00,     'isAcceptable': lambda x: (( 1.00   <= x)   and (x <= 10.00)),    'str_to_val': lambda x: round(float(x), 2), 'val_to_str': lambda x: f"{x:.2f}"},
+              {'name': 'delta',             'defaultValue': 0.0000,   'isAcceptable': lambda x: ((-1.0000 <= x)   and (x <= 1.0000)),   'str_to_val': lambda x: round(float(x), 4), 'val_to_str': lambda x: f"{x:.4f}"},
+              {'name': 'shortStrength',     'defaultValue': 1.000000, 'isAcceptable': lambda x: (( 0.000000 <= x) and (x <= 1.000000)), 'str_to_val': lambda x: round(float(x), 6), 'val_to_str': lambda x: f"{x:.6f}"},
+              {'name': 'longStrength',      'defaultValue': 1.000000, 'isAcceptable': lambda x: (( 0.000000 <= x) and (x <= 1.000000)), 'str_to_val': lambda x: round(float(x), 6), 'val_to_str': lambda x: f"{x:.6f}"},
+              {'name': 'downHillThreshold', 'defaultValue': 0.8000,   'isAcceptable': lambda x: (( 0.000000 <= x) and (x <= 1.000000)), 'str_to_val': lambda x: round(float(x), 4), 'val_to_str': lambda x: f"{x:.4f}"}]
 
 """
 [1]: params: (type: tuple)
@@ -68,44 +69,75 @@ def getTEF(params: tuple, linearizedAnalysis: dict, tcTracker_model: dict) -> fl
      param_delta,
      param_strength_SHORT,
      param_strength_LONG,
+     param_downHillThreshold,
     ) = params
+
+
 
     #[2]: Analysis Reference
     la_mmacd_msDeltaAbsMARel = linearizedAnalysis.get('0_MMACD_MSDELTAABSMAREL', None)
     if la_mmacd_msDeltaAbsMARel is None: return (None, 0)
 
+
+
     #[3]: TC Tracker
     #---[3-1]: Model Verification & Initialization
     if not tcTracker_model:
         tcTracker_model['tefVal_prev']                   = 0
+        tcTracker_model['tefVal_absMax']                 = 0
         tcTracker_model['la_mmacd_msDeltaAbsMARel_prev'] = 0
+        tcTracker_model['isDownhill']                    = False
+
     #---[3-2]: Cycle Check
     isShort_prev = (tcTracker_model['la_mmacd_msDeltaAbsMARel_prev'] < param_delta)
     isShort_this = (la_mmacd_msDeltaAbsMARel                         < param_delta)
     cycleReset   = (isShort_prev ^ isShort_this)
 
+
+
     #[4]: TEF Value Calculation
     #---[4-1]: Effective Parameter
     if isShort_this: param_strength_eff = param_strength_SHORT
     else:            param_strength_eff = param_strength_LONG
+    
     #---[4-2]: MSDeltaAbsMARel Normalization
     x_sign = -1.0 if la_mmacd_msDeltaAbsMARel < 0 else 1.0
     x_abs  = pow(abs(la_mmacd_msDeltaAbsMARel/param_alpha), param_beta)
     y_norm = math.tanh(x_abs)*x_sign
+
     #---[4-3]: TEF Value
     width = param_delta+1.0 if isShort_this else 1.0-param_delta
     dist  = abs(y_norm-param_delta)
-    tefVal_abs = max(1-dist/max(width, 1e-9), 0.0)*param_strength_eff
+    tefVal_abs = dist/max(width, 1e-9)*param_strength_eff
     if width == 0: tefVal_abs = 0.0
-    #---[4-4]: Cyclic Minimum
-    if not cycleReset: tefVal_abs = min(tefVal_abs, abs(tcTracker_model['tefVal_prev']))
-    #---[4-5]: Direction
+
+    #---[4-4]: TEF Value Cyclic Maximum
+    if cycleReset: tefVal_absMax = tefVal_abs
+    else:          tefVal_absMax = max(tcTracker_model['tefVal_absMax'], tefVal_abs)
+
+    #---[4-5]: Mode
+    if cycleReset: isDownhill = False
+    else:          isDownhill = tcTracker_model['isDownhill']
+    if tefVal_abs < tefVal_absMax*param_downHillThreshold:
+        isDownhill = True
+
+    #---[4-6]: Effective TEF Value
+    if isDownhill:
+        tefVal_abs = min(tefVal_abs, abs(tcTracker_model['tefVal_prev']))
+
+    #---[4-7]: Direction
     if isShort_this: tefVal = -tefVal_abs
     else:            tefVal =  tefVal_abs
 
+
+
     #[5]: TC Tracker Update
     tcTracker_model['tefVal_prev']                   = tefVal
+    tcTracker_model['tefVal_absMax']                 = tefVal_absMax
     tcTracker_model['la_mmacd_msDeltaAbsMARel_prev'] = la_mmacd_msDeltaAbsMARel
+    tcTracker_model['isDownhill']                    = isDownhill
+
+
 
     #[6]: Finally
     tefDir = 'SHORT' if isShort_this else 'LONG'
