c1fe8df78c1081e000b3799e3d651e444bf6768c
Bumsu Kim <kimlvis31@gmail.com>
2026-09-24 15:47:34 +0900

    fix: remove duplicate code

--- managers/workers/trade_manager/account.py
+++ managers/workers/trade_manager/account.py
@@ -1429,7 +1429,6 @@ class Account:
 
             #[2-4-1]: Order Type & Price Determination
             tc_orderType = tc['orderType']
-            tc_orderType = tc['orderType']
             if tc_orderType == 'ADAPTIVE': #'ADAPTIVE': LIMIT By Default, MARKET On A TEF Direction Reversal (CLEAR)
                 if th_type == 'CLEAR': tc_orderType = 'MARKET'
                 else:                  tc_orderType = 'LIMIT'
